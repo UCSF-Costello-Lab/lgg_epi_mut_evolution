39499fd288ba0815e1d3624cd2253e1af0d22943
Alex Pankov <pankov.alex@gmail.com>
2015-10-12 11:48:13 -0700

    extracting methylation-associated genes

---
+++ R/green_genes.R
@@ -0,0 +1,35 @@
+load("results/logit_limma_3.RData")
+load("results/log_limma_3.RData")
+
+p_cutoff <- .05
+go_p_cutoff <- .001
+
+load("data/annotations_with_gencode_1500_TSS_1000.RData", verbose=TRUE); anno <- anno_with_k_tss_p_named;
+
+log_df <- subset(log_df, rownames(log_df) %in% anno$genes[anno$IlmnID %in% rownames(logit_df)])
+
+fp_count_control <- function(p_methyl, p_gene_constant ){
+  temp <- length(p_methyl)/length(p_gene_constant)*p_methyl
+  temp[temp>1] <- 1
+  return(  temp )
+}
+
+p_methyl_adjusted <- fp_count_control(logit_df$temp, log_df$temp)
+
+cond_rna <- with(log_df, which(temp < p_cutoff & (g4_fc_log_2 <=log(1/2,2)) &  (g4_fc_log_2 - g2_fc_log_2) < -1))
+cond_methyl <- with(logit_df,which(p_methyl_adjusted < p_cutoff & (g4_beta_d >= .2)  & (g4_beta_d - g2_beta_d) > .15))
+
+
+dat <- log_df[cond_rna,  ]
+methyl_dat <- logit_df[cond_methyl ,  ]  
+
+cat(as.character(rownames(methyl_dat)), sep = '\n', file = 'results/sig_probes_green.txt')
+
+anno_in_sig_gene <- unique(anno[which(anno$genes %in% row.names(dat)),])
+sig_genes_hypeMehtyl_upExpr <- unique(anno_in_sig_gene[which(anno_in_sig_gene$IlmnID %in% row.names(methyl_dat)),"gene_name"])
+
+
+cat(as.character(sig_genes_hypeMehtyl_upExpr), sep = '\n', file = "results/M3_genes_green.txt")
+
+#cat(as.character(row.names(logit_df)), sep = '\n', file = "results/probe_bg.txt")
+#cat(as.character(row.names(log_df)), sep = '\n', file = 'results/rna_bg.txt')
